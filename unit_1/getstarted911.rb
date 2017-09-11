5b438af7d804f3769a915457a810056a5bbeccef
Natalie <nday2018@ihs.immaculateheart.org>
2017-09-11 09:01:01 -0700

    more get started

--- unit_1/getstarted911.rb
+++ unit_1/getstarted911.rb
@@ -1,10 +1,23 @@
+# puts "How many episodes have you watched?"
+# episodes = gets.chomp.to_i
+#
+# while !(episodes == 10 or episodes >= 10)
+#   puts "I have only watched #{episodes} episodes"
+#   puts "How many more episodes will you watch?"
+#   episodes = episodes + gets.chomp.to_i
+# end
+# puts "I have watched enough"
 
-puts "How many episodes have you watched?"
-episodes = gets.chomp.to_i
 
-while !(episodes == 10)
-  puts "I have only watched #{episodes} episodes"
-  puts "Time to watch another episode"
-  episodes = episodes + 1
+def greeting
+  puts "hi gaby"
+  puts "hi megan"
+  puts "hi briana"
+  puts "hi cameron"
 end
-puts "I have watched enough"
+
+def greet(name, age)
+  puts "Hello #{name}. " + "You are #{age * 365} days old"
+end
+
+greet("Natalie", 17)
